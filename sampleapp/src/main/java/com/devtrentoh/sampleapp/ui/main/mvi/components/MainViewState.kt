de8fdef5dad28859afb930c9f65b289b8a482fa2
Trent <devtrentoh@gmail.com>
2018-11-03 20:15:32 +0900

    Layouts & ViewStates

--- sampleapp/src/main/java/com/devtrentoh/sampleapp/ui/main/mvi/components/MainViewState.kt
+++ sampleapp/src/main/java/com/devtrentoh/sampleapp/ui/main/mvi/components/MainViewState.kt
@@ -3,4 +3,11 @@ package com.devtrentoh.sampleapp.ui.main.mvi.components
 import com.trent.simplemvi.mvi.components.MviViewState
 
 sealed class MainViewState : MviViewState {
+    data class TodoListViewState(val todoList: List<TodoItem>) : MviViewState
+    sealed class TextInputViewState(open val currentText: String) : MviViewState {
+        data class TodoAddViewState(override val currentText: String) : TextInputViewState(currentText)
+        data class TodoEditViewState(val editTarget: TodoItem, override val currentText: String): TextInputViewState(currentText)
+    }
 }
+
+data class TodoItem(val uniqueIndex: Int, val description: String, val isDone: Boolean = false)
