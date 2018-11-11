f1d9e9bace7ae1ced52515da60d035c68991139d
Trent <devtrentoh@gmail.com>
2018-11-12 01:07:22 +0900

    Remove bifunction dependency from Reducer

--- simplemvi/src/main/java/com/trent/simplemvi/mvi/MviReducerHolder.kt
+++ simplemvi/src/main/java/com/trent/simplemvi/mvi/MviReducerHolder.kt
@@ -5,5 +5,5 @@ import com.trent.simplemvi.mvi.components.MviViewState
 import io.reactivex.functions.BiFunction
 
 interface MviReducerHolder<R : MviResult, S : MviViewState> {
-    val resultReducer: BiFunction<S, R, S>
+    val resultReducer: Function2<S, R, S>
 }
