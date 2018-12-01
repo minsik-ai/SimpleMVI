ce8787b84813022b8f2e9e6f313207347c7ef902
Trent <devtrentoh@gmail.com>
2018-12-02 03:37:32 +0900

    MviFragment Added

--- sampleapp/src/main/java/com/devtrentoh/sampleapp/ui/main/MainFragment.kt
+++ sampleapp/src/main/java/com/devtrentoh/sampleapp/ui/main/MainFragment.kt
@@ -16,45 +16,26 @@
 
 package com.devtrentoh.sampleapp.ui.main
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.devtrentoh.sampleapp.R
 import com.devtrentoh.sampleapp.ui.main.mvi.MainProcessorHolder
 import com.devtrentoh.sampleapp.ui.main.mvi.MainReducerHolder
-import com.trent.simplemvi.MviViewModelFactory
+import com.devtrentoh.sampleapp.ui.main.mvi.components.MainIntent
+import com.devtrentoh.sampleapp.ui.main.mvi.components.MainResult
+import com.devtrentoh.sampleapp.ui.main.mvi.components.MainViewState
+import com.trent.simplemvi.MviFragment
 
-class MainFragment : Fragment() {
+class MainFragment : MviFragment<MainIntent, MainResult, MainViewState, MainViewModel>(
+    R.layout.main_fragment,
+    MainViewModel::class.java,
+    { MainProcessorHolder(MainModel()) },
+    { MainReducerHolder() },
+    { fragment, viewModel ->
+        MainView(fragment.activity, fragment.view, viewModel).apply {
+            setup()
+        }
+    }) {
 
     companion object {
         fun newInstance() = MainFragment()
     }
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(
-            this,
-            MviViewModelFactory(
-                MainProcessorHolder(MainModel()),
-                MainReducerHolder()
-            )
-        ).get(MainViewModel::class.java)
-        MainView(this.activity, this.view, viewModel).apply {
-            lifecycle.addObserver(this)
-            setup()
-        }
-    }
-
 }
